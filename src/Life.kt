fe97ccc4cdbae177c50cfd6a470734262b70d5c5
Ross Harper <ross.harper@bbc.co.uk>
2017-08-10 09:38:28 +0100

    Moving stuff around for fun

--- src/Life.kt
+++ src/Life.kt
@@ -1,20 +1,20 @@
-typealias Population = Set<Cell>
-fun populationOf(vararg cells: Cell) : Population = setOf(*cells)
 
 class Life(val population: Population) {
     fun evolved() = Life(population.survivors and population.newBirths)
 }
+
+// Population typealias and extensions
+typealias Population = Set<Cell>
+
+fun populationOf(vararg cells: Cell) : Population = setOf(*cells)
 
 private infix fun Population.and(population: Population) : Population = this.union(population)
 
 private val Population.survivors : Population
         get() = this.filter { it shouldSurviveEvolutionOfPopulation this }.toSet()
 
-private infix fun Cell.shouldSurviveEvolutionOfPopulation(population: Population)
-        = this hasTwoOrThreeNeighboursInPopulation population
-
-private infix fun Cell.hasTwoOrThreeNeighboursInPopulation(population: Population)
-        = (population livingNeighboursOf this).size in 2..3
+private val Population.newBirths: Population
+    get() = this.flatMap { this deadNeighboursOf it }.filter { (this livingNeighboursOf it).size == 3 }.toSet()
 
 private infix fun Population.livingNeighboursOf(cell: Cell) : Population
         = this.filter { it isNeighbourOf cell }.toSet()
@@ -22,11 +22,18 @@ private infix fun Population.livingNeighboursOf(cell: Cell) : Population
 private infix fun Cell.isNeighbourOf(cell:Cell)
         = cell.neighbours.contains(this)
 
-private val Population.newBirths: Population
-        get() = this.flatMap { this deadNeighboursOf it }.filter { (this livingNeighboursOf it).size == 3 }.toSet()
+
 
 private infix fun Population.deadNeighboursOf(cell: Cell) : Population
         = (cell.neighbours).filter { !this.contains(it) }.toSet()
+
+// Cell extensions
+
+private infix fun Cell.shouldSurviveEvolutionOfPopulation(population: Population)
+        = this hasTwoOrThreeNeighboursInPopulation population
+
+private infix fun Cell.hasTwoOrThreeNeighboursInPopulation(population: Population)
+        = (population livingNeighboursOf this).size in 2..3
 
 private val Cell.neighbours : Population
     get() =  setOf( this.above.left, this.above, this.above.right,
